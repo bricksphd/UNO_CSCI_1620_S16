66476050f351b0869ee93871370d0f4db3b6702d
Bricks <b.ricks@unomaha.edu>
2016-01-20 14:12:58 -0600

    Adding the explorer program for section 5.

---
+++ Explorer/Section5/Alien.java
@@ -0,0 +1,10 @@
+public class Alien{
+    
+    private String myDescription = "You gaze at yourself and notice that you have blue shorts on and big black eyes. I must be an alien, you think. You have fire powers now.";
+    
+    public String description()
+    {
+        return myDescription;
+    }
+    
+}
---
+++ Explorer/Section5/Me.java
@@ -0,0 +1,14 @@
+
+public class Me {
+
+	
+	public void Me()
+	{
+		
+	}
+	
+	public String description()
+	{
+		return "You are here. You feel great.";
+	}
+}
