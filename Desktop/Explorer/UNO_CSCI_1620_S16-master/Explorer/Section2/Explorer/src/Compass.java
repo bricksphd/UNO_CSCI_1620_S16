8bdfaaba903980c7473de35837ff1fc230648028
darthaustin96 <darthaustin96@192.168.0.15>
2016-01-22 13:26:43 -0600

    Austin Nielsen Minor Changes

---
+++ Desktop/Explorer/UNO_CSCI_1620_S16-master/Explorer/Section2/Explorer/src/Compass.java
@@ -0,0 +1,24 @@
+public class Compass {
+
+    public static final String COLOR_RESET = "\u001B[0m";
+    public static final String COLOR_BLACK = "\u001B[30m";
+    public static final String COLOR_RED = "\u001B[31m";
+    public static final String COLOR_GREEN = "\u001B[32m";
+    public static final String COLOR_YELLOW = "\u001B[33m";
+    public static final String COLOR_BLUE = "\u001B[34m";
+    public static final String COLOR_PURPLE = "\u001B[35m";
+    public static final String COLOR_CYAN = "\u001B[36m";
+    public static final String COLOR_WHITE = "\u001B[37m";
+    
+    public String description()
+    {
+        return "";
+    }
+    
+    public String drop() {
+        
+        return "Your " + COLOR_BLUE + "compass" + COLOR_RESET + " helps you keep your sense of direction. You cannot drop it.";
+        
+    }
+
+}
