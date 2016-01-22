7561af65a52f6faa57c6589d0248c7ff39a1ec66
AustinNielsen <austinnielsen@unomaha.edu>
2016-01-22 04:19:35 -0600

    Austin Nielsen Updated Shovel.java

--- Explorer/Section2/Shovel.java
+++ Explorer/Section2/Shovel.java
@@ -10,16 +10,47 @@ public class Shovel {
     public static final String COLOR_PURPLE = "\u001B[35m";
     public static final String COLOR_CYAN = "\u001B[36m";
     public static final String COLOR_WHITE = "\u001B[37m";
+    public static boolean shovelGrabbed = false;
+    
+    public static String GrabShovel =  "";
     
 	public String description()
 	{
-		return "I don't understand what to do with the shovel. Please try again.";
+		return "";
 	}
 	
 	public String grab()
 	{
-        Around.myDescription = "You find yourself at the end of a dirt road.  The only direction you are able to go is " + COLOR_BLUE + "east." + COLOR_RESET;
-		return "You grab the " + COLOR_RED + "shovel." + COLOR_RESET;
+        if(shovelGrabbed == false) {
+            Around.myDescription = "You find yourself at the end of a dirt road.  The only direction you are able to go is " + COLOR_BLUE + "east." + COLOR_RESET;
+            Inventory.fullInv += (String.valueOf(Inventory.invShovel));
+            shovelGrabbed = true;
+            GrabShovel = "You grab the " + COLOR_RED + "shovel." + COLOR_RESET;
+            return GrabShovel;
+        }
+        else {
+            GrabShovel = "You already have the " + COLOR_RED + "shovel." + COLOR_RESET;
+            return GrabShovel;
+        }
+        
 	}
+    
+    public String drop() {
+        
+        if(shovelGrabbed == true) {
+            Around.myDescription = "You find yourself at the end of a dirt road.  The only direction you are able to go is " + COLOR_BLUE + "east" + COLOR_RESET + ".  There is a " + COLOR_RED + "shovel " + COLOR_RESET + "near by.";
+            Inventory.fullInv = Inventory.fullInv.replaceAll(Inventory.invShovel, "");
+            shovelGrabbed = false;
+
+            GrabShovel = "You drop the " + COLOR_RED + "shovel." + COLOR_RESET;
+            return GrabShovel;
+        }
+        
+        else {
+            GrabShovel = "You don't have a " + COLOR_RED + "shovel." + COLOR_RESET;
+            return GrabShovel;
+        }
+        
+    }
 
 }
